949ac68bb04db2bba6ecd55f84dde172bb630a2c
Ti-22 <shubhamdbairagi@gmail.com>
2022-03-15 14:59:48 +0530

    change database name lexuxcumin -> sortboxs

--- config/databaseConfiguration.js
+++ config/databaseConfiguration.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/lexusCumin")
-.then(()=>{
-    console.log('Database is Connected!')
-})
-.catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect("mongodb://localhost:27017/sortboxs")
+  .then(() => {
+    console.log("Database is Connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
