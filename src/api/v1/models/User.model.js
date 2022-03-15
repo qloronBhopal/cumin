ab4926197df16254261479dc161a9ce5dafde2b2
Ti-22 <shubhamdbairagi@gmail.com>
2022-03-15 15:02:15 +0530

    added new field role in module

--- src/api/v1/models/User.model.js
+++ src/api/v1/models/User.model.js
@@ -1,40 +1,72 @@
 const mongoose = require("mongoose");
+const { userDefaultProfilePicUrl } = require("../../../../config/db_related_config");
 
 const userSchema = mongoose.Schema(
   {
-    username: {
+    name: {
       type: String,
-      required: true,
+      required: [true, "Name: Required!"],
       trim: true,
-      minlength: 3,
-      maxlength: 40
+      minLength: [3, "name: Min Length Required 3!"],
+      maxLength: [50, "name: Max Length Required 50!"]
     },
-    personal_email: {
+    email: {
       type: String,
-      required: true,
+      required: [true, "Email: Required!"],
+      lowercase: true,
       trim: true,
+      minLength: [6, "email: Min Length Required 6!"],
+      maxLength: [80, "email: Max Length Required 80!"],
       unique: true,
-      minlength: 6,
-      maxlength: 80
+      validate: [(email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email), "Invalid Email!"],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Invalid Email!"]
     },
     password: {
       type: String,
-      required: true
+      required: [true, "Password: required!"],
+      minLength: [6, "password: Min Length Required 6!"],
+      maxLength: [700, "password: Max Length Required 700!"]
     },
-    profile_picture: {
+    role: {
+      type: Number,
+      enum: {
+        values: [0, 1],
+        message: "{VALUE} is not supported!"
+      },
+      default: 0 // 0 = user, 1 = admin
+    },
+    status: {
+      type: Number,
+      enum: {
+        values: [0, 1],
+        message: "{VALUE} is not supported!"
+      },
+      default: 1 // 0 = Inactive, 1 = Active
+    },
+    avatar: {
       type: String,
-      required: false
+      default: userDefaultProfilePicUrl,
+      trim: true,
+      validate: [
+        (url) => ["pdf", "jpg", "png"].indexOf("asdas.png".split(".").pop().toLowerCase()) !== -1,
+        "Unsupported File Extension!"
+      ],
+      minLength: [4, "avatar: Min Length Required 4!"],
+      maxLength: [700, "avatar: Max Length Required 700!"]
     },
     mobile_number: {
       type: Number,
-      required: true,
-      minlength: 10,
+      required: [true, "Mobile Number Is Required!"],
+      minLength: [10, "Invalid Mobile Number!"],
+      maxLength: [13, "Invalid Mobile Number!"],
       unique: true
     },
     whatsapp_number: {
       type: Number,
       required: false,
-      minlength: 10
+      minLength: [10, "Invalid Mobile Number!"],
+      maxLength: [13, "Invalid Mobile Number!"],
+      unique: true
     },
     office_location: {
       type: Object,
@@ -55,14 +87,6 @@ const userSchema = mongoose.Schema(
     last_active: {
       type: Date,
       required: false
-    },
-    status: {
-      type: String,
-      enum: {
-        values: ["1", "0"],
-        message: "{VALUE} is not supported!"
-      },
-      default: "1"
     },
     deletion_reason: {
       type: String,
